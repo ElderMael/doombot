eee1684cfd417aea9021467c8c7d2a58e18f578f
ElderMael <sftwr.mael@gmail.com>
2017-11-16 22:34:47 -0600

    Auto Approve Terraform Plans

--- infrastructure/infrastructure.sh
+++ infrastructure/infrastructure.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
+set -ex
 
-if [ "$1" == "create" ]; then
+if [ -z "${1}"  ]; then
+
+fi
+
+
+if [ "${1}" == "create" ]; then
 
     terraform init
     terraform plan
 
-    terraform apply
+    terraform apply -auto-approve
 
 
 fi
 
-if [ "$1" == "destroy" ]; then
+if [ "${1}" == "destroy" ]; then
 
     terraform destroy --force
 
